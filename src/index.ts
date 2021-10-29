03e98803e7bcdb5870643d5373ae4d9acd443510
wangbei16 <wangbei6@jd.com>
2021-10-29 18:47:05 +0800

    增加测试用例

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,3 @@ import countdown from "./countdown";
 import formatRemaining  from './utils/formatRemaining'
 
 export default {countdown, formatRemaining}
-
-// export default function(incomingText: string) {
-//     return `${incomingText}-static`;
-// }
