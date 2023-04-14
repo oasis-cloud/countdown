9971150a42632074cbd8c50e6c6956260411879a
oasis-cloud <835865303@qq.com>
2023-04-14 13:23:23 +0800

    add: test ci

--- src/utils/formatRemaining.ts
+++ src/utils/formatRemaining.ts
@@ -3,5 +3,6 @@ export default function formatRemaining(remaining: string) {
     const hour = ((remainingNum / (60 * 60)) % 24).toString().padStart(2, '0')
     const min = ((remainingNum / (60)) % 60).toString().padStart(2, '0')
     const sec = ((remainingNum) % 60).toString().padStart(2, '0');
+    console.log('sec')
     return `${hour}:${min}:${sec}`;
 }
